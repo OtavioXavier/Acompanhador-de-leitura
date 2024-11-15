4acd9d65ce9964ffedd480a9e6a7e42df65fa25f
Guilherme Sampaio <g.sampaio2021@gmail.com>
2024-11-15 11:52:40 -0300

    feat: Added Splash Screen, application logo, internationalization and some simple settings.

--- app/src/main/java/com/example/acompanhadordeleitura/MainActivity.java
+++ app/src/main/java/com/example/acompanhadordeleitura/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.acompanhadordeleitura;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +19,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        getWindow().setStatusBarColor(Color.parseColor("#FFFFFF"));
+
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3400);
+        Toast.makeText(this, getString(R.string.title_toast_splashScreen), Toast.LENGTH_LONG).show();
     }
 }
